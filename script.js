d21a352b6ab0a68f9d84dc539c8c257332d8b4d1
Steven Chan <stevenchan@Stevens-MacBook-Air.local>
2024-07-18 11:44:41 -0700

    added completed average salary and random employee functions

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Get a reference to the #add-employees-btn element
 const addEmployeesBtn = document.querySelector('#add-employees-btn');
 
-//Array for the employee info
-let arrayofemployees = []
+//Array for the employee names and salary to be stored
+const arrayofemployees = []
 
 //Loop until user chooses to stop entering employees
 let enterInfo = true
@@ -10,40 +10,55 @@ let enterInfo = true
 // Collect employee data
 const collectEmployees = function() {
   // TODO: Get user input to create and return an array of employee objects
-  
+  // The "Prompt" method instructs the browser to display a dialog asking the user to imput text
+  // Office hours 7/17/24 - Instructor Luigi Campbell demonstrated using the parseFloat function on the salary variable.
+  // average salary was not calculating correctly for me. parseFloat was needed to turn salary into a floating point number. Adding parseFloat to line 19 resolved the issue. 
   while (enterInfo) {
-    let firstName = prompt(`Please enter the employee's first name`);
-    let lastName = prompt(`Plese enter the employee's last name`);
-    let salary = prompt(`Please enter the employee's salary`);
+    const firstName = prompt(`Please enter the employee's first name`);
+    const lastName = prompt(`Plese enter the employee's last name`);
+    const salary = parseFloat(prompt(`Please enter the employee's salary`));
 
+    //Per the challenge instructions, the salary must be entered as a number, otherwise it should default to 0
     if(isNaN(salary)) {
       salary = 0;
     };
 
-    //Creating object so the entered employee info can be pushed into the array
-    const employeeinfo = {
-      FirstName:firstName,
-      LastName:lastName,
-      Salary:salary
-    };
+    //Assisted by Xpert Learning Assistant
+    //Creating an object so that we can place all the entered employee info into one object then use the Push method to push the object into the array
+    const employeeinfo = {firstName, lastName, salary};
 
-    arrayofemployees.push(employeeinfo);
+    arrayofemployees.push(employeeinfo)
 
     //Ask user if they want to enter another employee
-    enterInfo = confirm('Would you like to enter another employee?');
+    //The "confirm" method instructs the browser to display a dialog and waits for the user to confirm or cancel the dialog
+    //If the user selects 'cancel' then that ends the loop
+    enterInfo = confirm(`Would you like to enter another employee?`);
 
   }
-  return arrayofemployees
+  return arrayofemployees;
 }
 
 // Display the average salary
 const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
+let totalSalary = 0
+for(let i=0; i < arrayofemployees.length; i++){
+  totalSalary += arrayofemployees[i].salary;
+  }
+
+  //The average salary was sometimes displaying too many numbers after the decimal.
+  //I found the 'toFixed' method after searching how to limit the number of decimal places displayed.
+  const avgSalary = (totalSalary / arrayofemployees.length).toFixed(2);
+  console.log(`The average salary of our ${arrayofemployees.length} employees is $${avgSalary}`)
 }
 
 // Select a random employee
+//Referenced script.js lines 50-54 of the solved student mini project in Module 3
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
+  const randEmploy = Math.floor(Math.random() * arrayofemployees.length)
+  const winner = arrayofemployees[randEmploy]
+  console.log(`Congratulations to ${winner.firstName} ${winner.lastName}, our random drawing winner!`)
 }
 
 /*
